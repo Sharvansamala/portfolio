108c29788546857ec22c73375d29ef41780130d5
Sharvansamala <sharvansamala2002@gmail.com>
2025-05-23 19:43:20 -0500

    Updated Home and About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,16 +14,15 @@ const About = () => {
                 </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-5xl font-bold '>
-                        <p>Hi. I'm Sharvan ,nice to meet you. Please take a look around.</p>
+                        <p>Hi. I'm Sharvan ,nice to meet you;</p>
                     </div>
                     <div className='sm:text-left text-1xl'>
-                        <p>I am a Full Stack Web Developer with a strong foundation in object-oriented programming, 
-                            currently pursuing a Master’s in Information Systems and Technology at the University of 
-                            North Texas. With expertise in AWS, Java, Spring Boot, MySQL and Go Lang. I focus 
-                            on creating scalable and efficient software solutions. My experience spans full-stack 
-                            development, microservices, and system design and I have successfully deployed multiple 
-                            projects on AWS. I am passionate about software design and continuously seek opportunities 
-                            to grow and contribute to innovative technologies.</p>
+                        <p>I am a Backend Developer with a strong foundation in object-oriented programming and expertise 
+                            in Java, Spring Boot, Go, MySQL, and AWS. I specialize in building scalable, high-performance 
+                            backend systems and microservices. With hands-on experience in designing robust architectures, 
+                            developing RESTful APIs, and deploying cloud-native applications on AWS, I focus on creating 
+                            efficient and reliable software solutions. Passionate about backend engineering and software 
+                            design, I continually seek opportunities to grow and contribute to innovative technologies.</p>
                     </div>
                 </div>
             </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,8 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-white text-2xl'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-8xl font-bold text-[#ccd6f6]'>Sharvan Samala</h1>
-                <h2 className='text-4xl sm:text-8xl font-bold text-[#8892b0]'>I'm a Software Engineer.</h2>
-                <p className='text-[#8992b0] py-5 max-w-700px text-2xl'>I am a Full Stack Web Developer with expertise in AWS, Java, Python and Microservices, 
-                    currently pursuing a Master’s in Information Systems and Technology.</p>
+                <h2 className='text-4xl sm:text-8xl font-bold text-[#8892b0]'>I'm a Backend Developer.</h2>
+                <p className='text-[#8992b0] py-5 max-w-700px text-2xl'>I am a Backend Developer with expertise in AWS, Java, Spring Boot and Microservices.</p>
                 <div>
                     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#15434e] hover:border-[#15434e]' >
                     <Link className='group' to="work" smooth={true} duration={500} >
